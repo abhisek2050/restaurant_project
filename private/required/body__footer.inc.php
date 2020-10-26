1c60976958f17d6df84ba8130e25ec440cd253ea
abhisek2050 <68422206+abhisek2050@users.noreply.github.com>
2020-10-26 18:48:13 +0530

    Create body__footer.inc.php

---
+++ private/required/body__footer.inc.php
@@ -0,0 +1,59 @@
+<div class="body__footer">
+            <div class="footer__header">
+                <img src="<?php base_url();?>image/logo-white.png" alt="image">
+            </div>
+
+            <div class="footer__header-content">
+                <ul class="address">
+                    <li><span class="span">Address:</span></li>
+                    <li><span class="creekside">Creekside Lane Avila Beach, CA 93424</span></li>
+                </ul>
+
+                <ul class="phone">
+                   <li><a href="tel+917978813614"><span class="span"> phone:</span></li>
+                    <li><span id="number">+7978813614</span></a></li>
+                </ul>
+
+                <ul class="e-mail">
+                   <li><a href="e-mail"><span class="span"> E-mail:</span></li>
+                   <li><span id="mail">abhisek@gmail.com</span></a></li>
+                </ul>
+
+                <div class="social__media-icons">
+                    <ul>
+                        <li>
+                            <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        </li></i>
+                        <li>
+                            <a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-vimeo" aria-hidden="true"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- end footer__header-contents -->
+
+            <div class="copy__right">
+                Copyright ©2020 All rights reserved
+            </div>
+        </div>
+        <!-- end body__footer -->
+
+    </div>
+    <!-- end body__wrap -->
+    <script src="<?php base_url();?>js/jquery.v.3.5.1.js"></script>
+    <script src="<?php base_url();?>js/bootstrap.js"></script>
+    <script src="<?php base_url();?>js/script.js"></script>
+</body>
+</html>
