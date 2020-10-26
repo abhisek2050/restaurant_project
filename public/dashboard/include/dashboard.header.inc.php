a7f66fe902dae6abf33c4d2bfd36ce90017c6268
abhisek2050 <68422206+abhisek2050@users.noreply.github.com>
2020-10-26 22:58:19 +0530

    Create dashboard.header.inc.php

---
+++ public/dashboard/include/dashboard.header.inc.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php base_url();?>css/bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="body__wrap">
+        <div class="body__header bg-dark py-4">
+        <nav class="container">
+            <ul class="nav d-flex">
+                <li><a class="text-light mr-5" href="<?php base_url();?>dashboard/testimonial/index.php">testimonials</a></li>
+                <li><a class="text-light mr-5" href="<?php base_url();?>dashboard/news/index.php">news</a></li>
+                <li><a class="text-light mr-5" href="<?php base_url();?>dashboard/service/index.php">service</a></li>
+                <li><a class="text-light mr-5" href="<?php base_url();?>dashboard/aboutus/index.php">aboutus</a></li>
+                <li><a class="text-light mr-5" href="<?php base_url();?>log_out.php">log out</a></li>
+                
+            </ul>
+        </nav>
+    </div>
+
+    
