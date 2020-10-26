f8f35182d29df9a44cf8964fc999eaea351a253f
abhisek2050 <68422206+abhisek2050@users.noreply.github.com>
2020-10-26 18:48:08 +0530

    Create db_connection.php

---
+++ private/config/db_connection.php
@@ -0,0 +1,9 @@
+   <!-- ****db connect*** -->
+   <?php
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_pwd = "";
+    $db_database = "sandbox";
+
+    $conn = mysqli_connect($db_host,$db_user,$db_pwd,$db_database);
+?>
