e3bd97df2f8d93897575109b0566f6bdbc7c8447
abhisek2050 <68422206+abhisek2050@users.noreply.github.com>
2020-10-26 18:49:11 +0530

    Create index.php

---
+++ public/about_us/index.php
@@ -0,0 +1,420 @@
+<?php
+
+    $title = "about_us";
+    include("../../private/config/config.php");
+    include("../../private/required/body__header.inc.php");
+?>
+
+<div class="body__banner">
+
+    <div class="banner__img">
+        <img src="<?php base_url()?>image/about__us-banner.jpg" alt="banner img">
+    </div>
+
+    <div class="banner__content">
+        
+        <div class="wrap__banner-content">
+            <span class="town">THE BEST IN TOWN </span>
+            <span class="blog">About our Pizzaria</span>
+        
+            <!-- <div class="banner__button">
+                <button>SEE TODAYS MENU</button>
+            </div> -->
+        </div>
+
+    </div>
+
+</div>
+<!-- end body__banner -->
+
+<div class="body__container">
+
+    <div class="image__section">
+
+        <div class="left__article wrap__container">
+            <img src="<?php base_url()?>image/about-1-600x400.jpg" alt="image">
+        </div>
+
+        <div class="right__article wrap__container ">
+            <img src="<?php base_url()?>image/about-2-600x400.jpg" alt="image">
+        </div>
+
+    </div>
+    <!-- end image__section -->
+
+    <div class="Lugais_section wrap__container">
+
+        <div class="header__lugais">
+            We are Lugais
+        </div>
+        <header class="header__lugais-content">
+            Maecenas fermentum tortor id fringilla molestie. In hac habitasse platea dictumst. Morbi maximus lobortis ipsum, ut blandit augue ullamcorper vitae. Nulla dignissim leo felis, eget cursus elit aliquet ut.
+        </header>
+        
+        <div class="left__right-content">
+
+            <div class="left__content">
+                    Maecenas fermentum tortor id fringilla molestie. In hac habitasse platea dictumst. Morbi maximus lobortis ipsum, ut blandit augue ullamcorper vitae. Nulla dignissim leo felis, eget cursus elit aliquet ut. Curabitur vel convallis massa. Morbi tellus ortor, luctus et lacinia non, tincidunt in lacus. Vivamus sed ligula imperdiet, feugiat magna vitae, blandit ex. Vestibulum id dapibus dolor, ac cursus nulla. 
+            </div>
+
+            <div class="right__content">
+                    Maecenas fermentum tortor id fringilla molestie. In hac habitasse platea dictumst.Morbi maximus lobortis ipsum, ut blandit augue ullamcorper vitae. Nulla dignissim leo felis, eget cursus elit aliquet ut. Curabitur vel convallismassa. Morbi tellus tortor, luctus et lacinia non, tincidunt in lacus. Vivamus sed ligula imperdiet, feugiat magna vitae, blandit ex. Vestibulumidda pibus dolor, accursus nulla.
+            </div>
+
+        </div>
+        <!-- end left__right-content -->
+
+        <footer class="footer__lugais">
+
+            <div class="left__button">
+                <button>
+                    SEE TODAYS MENU
+                </button>
+            </div>
+
+            <div class="right__button">
+                <button>
+                    ORDER NOW
+                </button>
+            </div>
+
+        </footer>
+        <!-- end footer__lugais -->
+
+    </div>
+    <!-- end Lugais_section -->
+
+    <div class="client__section bg-success">
+
+        <header class="client__header section__header">
+            What Clients Say
+        </header>
+
+        <div class="client__body testimonial__slider">
+            <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner container">
+                <div class="carousel-item active">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <article class="amazing__pizza">
+
+                                <div class="article__header">
+                                    Amazing Pizza
+                                </div>
+
+                                <div class="article__body">
+                                    Etiam nec odio vestibulum est mattis
+                                    efficiturut magna.Pellentesquesit amet tellus blandit. Etiam nec
+                                    odio vestibulum est mattis
+                                    effic iturut magna. Pellentesque sit am et tellus blandit.
+                                    Etiamnec odio vestibul. 
+                                </div>
+
+                                <div class="article__footer">
+                                    <figure>
+                                        <img src="../image/quoto-1-200x200.jpg" alt="image">
+                                    </figure>
+                                    <figcaption> Daiane Smith,Customer</figcaption>
+                                </div>
+
+                            </article>
+                        </div>
+                        <div class="col-sm-6">
+                            <article class="amazing__pizza">
+
+                                <div class="article__header">
+                                    Amazing Pizza
+                                </div>
+
+                                <div class="article__body">
+                                    Etiam nec odio vestibulum est mattis
+                                    efficiturut magna.Pellentesquesit amet tellus blandit. Etiam nec
+                                    odio vestibulum est mattis
+                                    effic iturut magna. Pellentesque sit am et tellus blandit.
+                                    Etiamnec odio vestibul. 
+                                </div>
+
+                                <div class="article__footer">
+                                    <figure>
+                                        <img src="../image/quoto-1-200x200.jpg" alt="image">
+                                    </figure>
+                                    <figcaption> Daiane Smith,Customer</figcaption>
+                                </div>
+
+                            </article>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <article class="amazing__pizza">
+
+                                <div class="article__header">
+                                    Amazing Pizza
+                                </div>
+
+                                <div class="article__body">
+                                    Etiam nec odio vestibulum est mattis
+                                    efficiturut magna.Pellentesquesit amet tellus blandit. Etiam nec
+                                    odio vestibulum est mattis
+                                    effic iturut magna. Pellentesque sit am et tellus blandit.
+                                    Etiamnec odio vestibul. 
+                                </div>
+
+                                <div class="article__footer">
+                                    <figure>
+                                        <img src="../image/quoto-1-200x200.jpg" alt="image">
+                                    </figure>
+                                    <figcaption> Daiane Smith,Customer</figcaption>
+                                </div>
+
+                            </article>
+                        </div>
+                        <div class="col-sm-4">
+                            <article class="amazing__pizza">
+
+                                <div class="article__header">
+                                    Amazing Pizza
+                                </div>
+
+                                <div class="article__body">
+                                    Etiam nec odio vestibulum est mattis
+                                    efficiturut magna.Pellentesquesit amet tellus blandit. Etiam nec
+                                    odio vestibulum est mattis
+                                    effic iturut magna. Pellentesque sit am et tellus blandit.
+                                    Etiamnec odio vestibul. 
+                                </div>
+
+                                <div class="article__footer">
+                                    <figure>
+                                        <img src="../image/quoto-1-200x200.jpg" alt="image">
+                                    </figure>
+                                    <figcaption> Daiane Smith,Customer</figcaption>
+                                </div>
+
+                            </article>
+                        </div>
+                        
+                        <div class="col-sm-4">
+                            <article class="amazing__pizza">
+
+                                <div class="article__header">
+                                    Amazing Pizza
+                                </div>
+
+                                <div class="article__body">
+                                    Etiam nec odio vestibulum est mattis
+                                    efficiturut magna.Pellentesquesit amet tellus blandit. Etiam nec
+                                    odio vestibulum est mattis
+                                    effic iturut magna. Pellentesque sit am et tellus blandit.
+                                    Etiamnec odio vestibul. 
+                                </div>
+
+                                <div class="article__footer">
+                                    <figure>
+                                        <img src="../image/quoto-1-200x200.jpg" alt="image">
+                                    </figure>
+                                    <figcaption> Daiane Smith,Customer</figcaption>
+                                </div>
+
+                            </article>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <article class="amazing__pizza">
+
+                                <div class="article__header">
+                                    Amazing Pizza
+                                </div>
+
+                                <div class="article__body">
+                                    Etiam nec odio vestibulum est mattis
+                                    efficiturut magna.Pellentesquesit amet tellus blandit. Etiam nec
+                                    odio vestibulum est mattis
+                                    effic iturut magna. Pellentesque sit am et tellus blandit.
+                                    Etiamnec odio vestibul. 
+                                </div>
+
+                                <div class="article__footer">
+                                    <figure>
+                                        <img src="../image/quoto-1-200x200.jpg" alt="image">
+                                    </figure>
+                                    <figcaption> Daiane Smith,Customer</figcaption>
+                                </div>
+
+                            </article>
+                        </div>
+                        <div class="col-sm-3">
+                            <article class="amazing__pizza">
+
+                                <div class="article__header">
+                                    Amazing Pizza
+                                </div>
+
+                                <div class="article__body">
+                                    Etiam nec odio vestibulum est mattis
+                                    efficiturut magna.Pellentesquesit amet tellus blandit. Etiam nec
+                                    odio vestibulum est mattis
+                                    effic iturut magna. Pellentesque sit am et tellus blandit.
+                                    Etiamnec odio vestibul. 
+                                </div>
+
+                                <div class="article__footer">
+                                    <figure>
+                                        <img src="../image/quoto-1-200x200.jpg" alt="image">
+                                    </figure>
+                                    <figcaption> Daiane Smith,Customer</figcaption>
+                                </div>
+
+                            </article>
+                        </div>
+                        <div class="col-sm-3">
+                            <article class="amazing__pizza">
+
+                                <div class="article__header">
+                                    Amazing Pizza
+                                </div>
+
+                                <div class="article__body">
+                                    Etiam nec odio vestibulum est mattis
+                                    efficiturut magna.Pellentesquesit amet tellus blandit. Etiam nec
+                                    odio vestibulum est mattis
+                                    effic iturut magna. Pellentesque sit am et tellus blandit.
+                                    Etiamnec odio vestibul. 
+                                </div>
+
+                                <div class="article__footer">
+                                    <figure>
+                                        <img src="../image/quoto-1-200x200.jpg" alt="image">
+                                    </figure>
+                                    <figcaption> Daiane Smith,Customer</figcaption>
+                                </div>
+
+                            </article>
+                        </div>
+                        <div class="col-sm-3">
+                            <article class="amazing__pizza">
+
+                                <div class="article__header">
+                                    Amazing Pizza
+                                </div>
+
+                                <div class="article__body">
+                                    Etiam nec odio vestibulum est mattis
+                                    efficiturut magna.Pellentesquesit amet tellus blandit. Etiam nec
+                                    odio vestibulum est mattis
+                                    effic iturut magna. Pellentesque sit am et tellus blandit.
+                                    Etiamnec odio vestibul. 
+                                </div>
+
+                                <div class="article__footer">
+                                    <figure>
+                                        <img src="../image/quoto-1-200x200.jpg" alt="image">
+                                    </figure>
+                                    <figcaption> Daiane Smith,Customer</figcaption>
+                                </div>
+
+                            </article>
+                        </div>
+                    </div>
+                </div>
+                <!-- end carousel-item -->
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
+    <div class="counter__section wrap__container bg-light">
+
+        <article class="counter first">
+
+            <figure class="counter__figure">
+                <img src="<?php base_url()?>image/icon1.png" alt="icon1">
+            </figure>
+
+            <div class="counter__body">
+                574
+            </div>
+
+            <div class="counter__footer">
+                Pizza per day
+            </div>
+
+        </article>
+
+        <article class="counter">
+
+            <figure class="counter__figure">
+                <img src="<?php base_url()?>image/icon4.png" alt="icon1">
+            </figure>
+
+            <div class="counter__body second">
+                14
+            </div>
+
+            <div class="counter__footer">
+                Sea Food Dishes
+            </div>
+
+        </article>
+
+        <article class="counter">
+
+            <figure class="counter__figure">
+                <img src="<?php base_url()?>image/icon3.png" alt="icon1">
+            </figure>
+
+            <div class="counter__body third">
+                03
+            </div>
+
+            <div class="counter__footer pasta_chefs">
+                Pasta Chefs
+            </div>
+
+        </article>
+
+        <article class="counter">
+
+            <figure class="counter__figure">
+                <img src="<?php base_url()?>image/icon2.png" alt="icon1">
+            </figure>
+
+            <div class="counter__body fourth">
+                52
+            </div>
+
+            <div class="counter__footer">
+                Salads per day
+            </div>
+
+        </article>
+
+    </div> 
+    <!-- end counter__section -->
+
+</div>
+<!-- end body__container -->
+
+<?php
+    include("../../private/required/body__footer.inc.php");
+?>
